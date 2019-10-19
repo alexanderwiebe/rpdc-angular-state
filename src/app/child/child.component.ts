bc220793d2a623823edd53a6fc8aded5ca428bf6
Alexander <alexanderjwiebe@gmail.com>
2019-10-19 13:41:39 -0600

    getting initial data setup

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -7,7 +7,16 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class ChildComponent implements OnInit {
+  currentValue = 0;
   constructor() {}
 
   ngOnInit() {}
+
+  inc() {
+    this.currentValue += 1;
+  }
+
+  dec() {
+    this.currentValue -= 1;
+  }
 }
