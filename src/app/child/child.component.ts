fc2555970638799ffeb3d6fd5251e194632a286f
Alexander <alexanderjwiebe@gmail.com>
2019-10-19 11:11:40 -0600

    main component structure is done

---
+++ src/app/child/child.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class ChildComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
