456ada50a00aaf648f42169b810361a65e1a810a
Alexander <alexanderjwiebe@gmail.com>
2019-10-19 21:07:07 -0600

    updates to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ParentComponent } from './parent/parent.component';
-import { ChildComponent } from './child/child.component';
+import { ParentFormComponent } from './components/parent/parent.component';
+import { ChildFormComponent } from './components/child/child.component';
+import { ParentComponent } from './containers/parent/parent.component';
+import { ChildComponent } from './containers/child/child.component';
 
 @NgModule({
-  declarations: [AppComponent, ParentComponent, ChildComponent],
+  declarations: [
+    AppComponent,
+    ParentFormComponent,
+    ChildFormComponent,
+    ParentComponent,
+    ChildComponent
+  ],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
